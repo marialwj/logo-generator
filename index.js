28c13e3144916706a6d9fda978188f2dda5d3e10
marialwj <mariaa.georgy@gmail.com>
2024-02-08 18:56:50 -0800

    changes in all files. ERROR- FIX ASAP

--- index.js
+++ index.js
@@ -1,52 +1,32 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
-const { Circle, Square, Triangle } = require('./lib/shapes');
-const questions = require('./lib/questions');
+import { Circle, Square, Triangle } from './lib/shapes.js';
+import questions from './lib/questions.js';
+import inquirer from 'inquirer';
 
-const init = () => {
-    inquirer.createPromptModule(questions)
-    .then((data) => {
-        console.log("Processing SVG File...");
-        switch (data.shape) {
-            case 'circle':
-                console.log('Circle is being created...');
-                const circle = new Circle(data.fill, data.stroke, data.strokeWidth, data.text, data.textColor, data.width);
-                fs.writeFile('circle.svg', circle.render(), (err) => { 
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log('Congratulations! Circle SVG File created successfully!');
-                    }
-                });
-                break;
-            case 'triangle':
-                console.log('Triangle is being created...');
-                const triangle = new Triangle(data.fill, data.stroke, data.strokeWidth, data.text, data.textColor, data.width);
-                fs.writeFile('triangle.svg', triangle.render(), (err) => { 
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log('Congratulations! Triangle SVG File created successfully!');
-                    }
-                });
-                break;
-            case 'square':
-                console.log('Square is being created...');
-                const square = new Square(data.fill, data.stroke, data.strokeWidth, data.text, data.textColor, data.width);
-                fs.writeFile('square.svg', square.render(), (err) => { 
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log('Congratulations! Square SVG File created successfully!');
-                    }
-                });
-                break;
-            default:
-                console.log('Invalid shape.');
-        }
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
-};
-                                               
+function validateHexCode(input) {
+  const hexColorRegex = /^#([0-9A-Fa-f]{3}){1,2}$/;
+  return hexColorRegex.test(input) ? true : 'Please enter a valid hex color code.';
+}
+
+async function generateShape() {
+  const answers = await inquirer.prompt(questions);
+
+  switch (answers.shape.toLowerCase()) {
+    case 'circle':
+      const circle = new Circle(answers.fill, answers.stroke, answers.strokeWidth, answers.text, answers.textColor, answers.radius);
+      console.log(circle.renderCircle());
+      break;
+    case 'square':
+      const square = new Square(answers.fill, answers.stroke, answers.strokeWidth, answers.text, answers.textColor, answers.sideLength);
+      console.log(square.renderSquare());
+      break;
+    case 'triangle':
+      const triangle = new Triangle(answers.fill, answers.stroke, answers.strokeWidth, answers.text, answers.textColor, answers.base, answers.height);
+      console.log(triangle.renderTriangle());
+      break;
+    default:
+      console.log('Invalid shape.');
+      break;
+  }
+}
+
+generateShape();
