2f42d423866757124d996c890b5a54f21162b99a
marialwj <mariaa.georgy@gmail.com>
2024-02-12 18:31:33 -0800

    fixed all errors and OOP now works, readme is still in progress as well

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
-import { Circle, Square, Triangle } from './lib/shapes.js';
-import questions from './lib/questions.js';
-import inquirer from 'inquirer';
+const {Circle, Square, Triangle} = require('./lib/shapes.js');
+// const questions = require('./lib/questions.js');
+const inquirer = require('inquirer');
+const fs = require('fs');
+const questions = [
+  {
+      type: "list",
+      name: "shape",
+      message: "What shape do you want to generate?",
+      choices: ["Circle", "Square", "Triangle"],
+  },
+
+  {
+      type: "input",
+      name: "text",
+      message: "What text do you want to add to the shape? (3 chars. max)",
+  },
+
+  {
+      type: "input",
+      name: "textColor",
+      message: "What color do you want the text to be? (enter the Hex code)",
+  }, 
+  
+  {
+      type: "input",
+      name: "fill",
+      message: "What background color do you want the shape to have? (enter the Hex code)",
+  },
+
+  {
+      type: "input",
+      name: "stroke",
+      message: "What color of the shape border should be? (enter the Hex code",
+  },
+
+  {
+      type: "number",
+      name: "strokeWidth",
+      message: "What width of the shape border should be? (# between 1-5)",
+  },
+
+];
 
 function validateHexCode(input) {
   const hexColorRegex = /^#([0-9A-Fa-f]{3}){1,2}$/;
@@ -9,24 +49,35 @@ function validateHexCode(input) {
 
 async function generateShape() {
   const answers = await inquirer.prompt(questions);
-
+   let shape;
+  
   switch (answers.shape.toLowerCase()) {
     case 'circle':
-      const circle = new Circle(answers.fill, answers.stroke, answers.strokeWidth, answers.text, answers.textColor, answers.radius);
-      console.log(circle.renderCircle());
+      shape = new Circle(answers.fill, answers.stroke, answers.strokeWidth, answers.text, answers.textColor, answers.radius);
       break;
     case 'square':
-      const square = new Square(answers.fill, answers.stroke, answers.strokeWidth, answers.text, answers.textColor, answers.sideLength);
-      console.log(square.renderSquare());
+      shape = new Square(answers.fill, answers.stroke, answers.strokeWidth, answers.text, answers.textColor, answers.sideLength);
       break;
     case 'triangle':
-      const triangle = new Triangle(answers.fill, answers.stroke, answers.strokeWidth, answers.text, answers.textColor, answers.base, answers.height);
-      console.log(triangle.renderTriangle());
+      shape = new Triangle(answers.fill, answers.stroke, answers.strokeWidth, answers.text, answers.textColor, answers.base, answers.height);
       break;
     default:
       console.log('Invalid shape.');
-      break;
+      return;
   }
-}
+
+  shape.setColor(answers.fill);
+   shape.setTextColor(answers.textColor);
+   shape.setText(answers.text);
+    const text = shape.render();
+
+
+    fs.writeFile('logo.svg', text, (err) => {
+      console.log(err);
+    }
+    )
+
+  }
+
 
 generateShape();
